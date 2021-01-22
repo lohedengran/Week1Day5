bac78536583f66a4421f41455b66c25b54ea1c60
Lo Hedengran <lohedengran@gmail.com>
2021-01-22 13:41:11 +0100

    Initial commit

---
+++ Week1Day5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Week1Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] characters = new char[5] { 'b', 'e', 'a', 'c', 'd' };
+            SelectionSort(characters);
+        }
+
+        private static void SelectionSort(char[] characters)
+        {
+            for (int i = 0; i < characters.Length -1; i++)
+            {
+                int lvi = i;
+                for (int j = i + 1; j < characters.Length; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
