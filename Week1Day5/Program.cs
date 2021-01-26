f2a0cc0e186ad12ab704029b471e0954c82e0704
Lo Hedengran <lohedengran@gmail.com>
2021-01-26 10:32:49 +0100

    Update Program.cs

--- Week1Day5/Program.cs
+++ Week1Day5/Program.cs
@@ -8,16 +8,31 @@ namespace Week1Day5
         {
             char[] characters = new char[5] { 'b', 'e', 'a', 'c', 'd' };
             SelectionSort(characters);
+            for (int i = 0; i < characters.Length; i++)
+            {
+                Console.WriteLine(characters[i]);
+            }
         }
 
         private static void SelectionSort(char[] characters)
         {
-            for (int i = 0; i < characters.Length -1; i++)
+            for (int i = 0; i < characters.Length - 1; i++)
             {
                 int lvi = i;
+
+
                 for (int j = i + 1; j < characters.Length; j++)
                 {
-
+                    if (characters[j] < characters[lvi])
+                    {
+                        lvi = j;
+                    }
+                }
+                if (lvi != i)
+                {
+                    char tmp = characters[i];
+                    characters[i] = characters[lvi];
+                    characters[lvi] = tmp;
                 }
             }
         }
